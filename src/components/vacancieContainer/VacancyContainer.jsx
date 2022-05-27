cfa1dcfd281d1f9c81b7890e15cbd85c868f1ff9
higashi04 <cesar.higashi04@gmail.com>
2022-05-27 11:16:07 -0500

    finished vacancy container

--- src/components/vacancieContainer/VacancyContainer.jsx
+++ src/components/vacancieContainer/VacancyContainer.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import { Collapse } from "react-bootstrap";
+
+import "./VacancyContainer.css";
 
 const VacancyContainer = (props) => {
+  const [collapse, setCollapse] = useState(false);
+  const [sign, setSign] = useState(true);
+
+  const signToggle = () => {
+    setCollapse(!collapse);
+    setSign(!sign);
+  };
+
   return (
-    <div className="container">
+    <div className="container vacancy-container-box my-3">
       <div className="row">
-        <h1>{props.position}</h1>
+        <div className="col-6">
+          <h1>{props.position}</h1>
+        </div>
+        <div className="col-6">
+          <button className="collBtn" onClick={() => signToggle()}>
+            {" "}
+            {sign ? "+" : "-"}{" "}
+          </button>
+        </div>
       </div>
-      <div className="row">
-        {`Edades de ${props.ageMin} a ${props.ageMax} años`}
-      </div>
+      <Collapse in={collapse}>
+        <div className="row">
+          <div id="collapsePanel" className="vacancy-requirements">
+            <ul>
+              <li>Género: {props.gender}</li>
+              <li>{`Edades de ${props.ageMin} a ${props.ageMax} años`}</li>
+              <li>{props.requirements}</li>
+            </ul>
+          </div>
+        </div>
+      </Collapse>
     </div>
   );
 };
--- src/pages/Vacancies/Vacancies.jsx
+++ src/pages/Vacancies/Vacancies.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-
+import Spinner from "react-bootstrap/Spinner";
 import VacancyContainer from "../../components/vacancieContainer/VacancyContainer";
+import "./Vacancies.css";
 
 const Vacancies = () => {
   const [data, setData] = useState([]);
@@ -23,10 +24,28 @@ const Vacancies = () => {
   useEffect(() => {
     handleData();
   }, []);
+  const correctCapitalize = (str) => {
+    const string = str.toLowerCase();
+    const stringTwo = string.charAt(0).toUpperCase() + string.slice(1);
+    return stringTwo;
+  };
   return (
     <>
-      <h1>Vacancies</h1>
-      {data.map(item => <VacancyContainer key={item._id} position={item.position} ageMin={item.ageStart} ageMax={item.ageEnd} /> )}
+      <h1 className="vacancy-h1 mt-3">Vacantes</h1>
+      {data ? (
+        data.map((item) => (
+          <VacancyContainer
+            key={item._id}
+            position={item.position}
+            ageMin={item.ageStart}
+            ageMax={item.ageEnd}
+            gender={item.gender}
+            requirements={correctCapitalize(item.requirements)}
+          />
+        ))
+      ) : (
+        <Spinner animation="border" />
+      )}
     </>
   );
 };
