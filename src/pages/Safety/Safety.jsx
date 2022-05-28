573fd615b23b0c553353e4321a536c24255fce85
higashi04 <cesar.higashi04@gmail.com>
2022-05-28 11:48:36 -0500

    refactored safety page to replace massive html file with reusable component

--- src/pages/Safety/Safety.jsx
+++ src/pages/Safety/Safety.jsx
@@ -1,7 +1,16 @@
 import React, { useEffect } from "react";
 import "./Safety.css";
+import TextContainer from "../../components/TextContainer/TextContainer";
 
 const Safety = () => {
+  const safetyArr = [
+    {id: 1, text: 'La seguridad forma parte integral de la responsabilidad de cada puesto de trabajo.'},
+    {id: 2, text: 'Trabajaremos para eliminar cabalmente y de forma definitiva las fatalidades, las lesiones, y las discapacidades originadas por accidente de trabajo.'},
+    {id: 3, text: 'Proveeremos un ambiente seguro, así como el equipo de protección necesario para prevenir las enfermedades ocupacionales.'},
+    {id: 4, text: 'Proveeremos los sistemas, controles, procedimientos, estándares y equipos, así como la capacitación necesaria para mantener en condiciones de seguridad optima todos los procesos.'},
+    {id: 5, text: 'Se realizaran auditorias internas y externas necesarias para el monitoreo del desempeño de seguridad y así promover la mejora continua en los procesos de detención, eliminación y control de riesgos inherentes en los lugares de trabajo.'},
+    {id: 6, text: 'Toda persona que Ingrese a nuestras instalaciones como cliente, proveedor o visitante deberá conocer las normas, reglamentos y procedimientos de seguridad aplicables al desempeño de sus labores dentro de la empresa.'},
+  ]
   useEffect(() => {
     document.body.style.backgroundColor = "#669BC7";
   });
@@ -16,55 +25,9 @@ const Safety = () => {
         </p>
         <h2 className="safety-h2">Por ello establecemos lo siguiente:</h2>
       </main>
-      <section>
-        <div className="container mb-5">
-          <div className="row">
-            <div className="col-4">
-              <p className="safety-p">
-                La seguridad forma parte integral de la responsabilidad de cada
-                puesto de trabajo.
-              </p>
-            </div>
-            <div className="col-4">
-              <p className="safety-p">
-                Trabajaremos para eliminar cabalmente y de forma definitiva las
-                fatalidades, las lesiones, y las discapacidades originadas por
-                accidente de trabajo.
-              </p>
-            </div>
-            <div className="col-4">
-              <p className="safety-p">
-                Proveeremos un ambiente seguro, así como el equipo de protección
-                necesario para prevenir las enfermedades ocupacionales.
-              </p>
-            </div>
-          </div>
-          <div className="row mb-5">
-            <div className="col-4">
-              <p className="safety-p">
-                Proveeremos los sistemas, controles, procedimientos, estándares
-                y equipos, así como la capacitación necesaria para mantener en
-                condiciones de seguridad optima todos los procesos.
-              </p>
-            </div>
-            <div className="col-4">
-              <p className="safety-p fifth-p">
-                Se realizaran auditorias internas y externas necesarias para el
-                monitoreo del desempeño de seguridad y así promover la mejora
-                continua en los procesos de detención, eliminación y control de
-                riesgos inherentes en los lugares de trabajo.
-              </p>
-            </div>
-            <div className="col-4">
-              <p className="safety-p">
-                Toda persona que Ingrese a nuestras instalaciones como cliente,
-                proveedor o visitante deberá conocer las normas, reglamentos y
-                procedimientos de seguridad aplicables al desempeño de sus
-                labores dentro de la empresa.{" "}
-              </p>
-            </div>
-          </div>
-        </div>
+      <section className="safety-section">
+        {safetyArr.map((item) => 
+          <TextContainer key={item.id} text={item.text} />)}
       </section>
     </>
   );
