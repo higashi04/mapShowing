2160aa1363595666414f8d5b410336b5b6c91b41
higashi04 <cesar.higashi04@gmail.com>
2022-06-08 10:35:09 -0500

    added drivers per company, where drivers are rendered according to user.company in global state from redux

--- src/components/Driver-box/Driver-Box.jsx
+++ src/components/Driver-box/Driver-Box.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Driver-box.css";
+import { Link } from "react-router-dom";
 
 const DriverBox = (props) => {
   return (
-    <div className="container driver-container mx-3">
+    <div className="container driver-container mx-3" >
       <div className="row mx-3">
         <div className="col-6">Nombre:</div>
         <div className="col-6">{props.name}</div>
+      </div>
+      <div className="row mt-3 driver-container-row">
+        <Link to={`/choferes/${props.id}`} className="btn btn-info btn-custom">Ver datos</Link>
       </div>
     </div>
   );
--- src/pages/Drivers/Drivers.jsx
+++ src/pages/Drivers/Drivers.jsx
@@ -5,25 +5,59 @@ import axios from "axios";
 import DriverBox from "../../components/Driver-box/Driver-Box";
 
 const Drivers = () => {
-  const [data, setData] = useState("");
-
-  const getDrivers = async() => {
-    try {
-
-    } catch(e) {
-      console.error(e)
-    }
-  }
+  const { user } = useSelector((state) => state.auth);
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     document.body.style.backgroundColor = "#669BC7";
   });
+  useEffect(() => {
+    const getDrivers = () => {
+      let url;
+      switch (user.company.toLowerCase()) {
+        case "medline":
+          url =
+            "https://transportes-villarreal.herokuapp.com/drivers/getDrivers/medline";
+          break;
+        case "oes":
+          url =
+            "https://transportes-villarreal.herokuapp.com/drivers/getDrivers/oes";
+          break;
+        case "bpi":
+          url =
+            "https://transportes-villarreal.herokuapp.com/drivers/getDrivers/bpi";
+          break;
+        case "aistermi":
+          url =
+            "https://transportes-villarreal.herokuapp.com/drivers/getDrivers/aistermi";
+          break;
+        default:
+          url = "https://transportes-villarreal.herokuapp.com/drivers/getDrivers";
+          break;
+      }
+      try {
+        const options = { method: "GET", url: url };
+        axios
+          .request(options)
+          .then((res) => setData(res.data))
+          .catch(function (error) {
+            console.error(error);
+          });
+      } catch (e) {
+        console.error(e);
+      }
+    };
+    getDrivers()
+  }, [user.company])
 
   return (
-    <>
+    <main className="pb-5">
       <h1 className="offset-md-5">Choferes</h1>
-      <DriverBox name='poyo' />
-    </>
+
+      {data.map((item) => (
+        <DriverBox key={item._id} name={item.name} id={item._id} />
+      ))}
+    </main>
   );
 };
 
